c65c933fcdc79bf62d490e7565a2bb7a691a3d23
wheresmehat <csstogo@gmail.com>
2016-10-06 11:54:31 +0200

    Add files via upload

---
+++ Intermediate%20Algorithm%20Scripting/Boo hoo.js
@@ -0,0 +1,57 @@
+﻿function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  
+	if (bool === true || bool === false) {
+  	
+  		console.log(true);
+  	
+  	}
+  	else {
+  		
+  		console.log(false);
+  		
+  	}
+  
+}
+
+
+booWho(true);		// true
+booWho(false);		// true
+booWho(null);		// false
+booWho([1, 2, 3]);	// false
+booWho({ "a": 1 });	// false
+booWho(NaN);		// false
+booWho("a");		// false
+booWho("true");		// false
+
+
+
+
+// using typeof
+/*
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  
+	if (typeof(bool) === "boolean") {
+  	
+  		console.log(true);
+  	
+  	}
+  	else {
+  		
+  		console.log(false);
+  		
+  	}
+  
+}
+
+
+booWho(true);		// true
+booWho(false);		// true
+booWho(null);		// false
+booWho([1, 2, 3]);	// false
+booWho({ "a": 1 });	// false
+booWho(NaN);		// false
+booWho("a");		// false
+booWho("true");		// false
+*/
