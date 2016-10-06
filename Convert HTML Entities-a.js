816c45f0dccff254840c452448c665d342d3b3cf
wheresmehat <csstogo@gmail.com>
2016-10-06 11:58:02 +0200

    Add files via upload

---
+++ Convert HTML Entities-a.js
@@ -0,0 +1,28 @@
+﻿function convertHTML(str) {
+  
+	var replaceObj = {
+		
+		"&": "&amp;",
+		"<": "&lt;",
+		">": "&gt;",
+		'"': "&quot;",
+		"'": "&apos;"
+	};
+	
+	
+	var res = str.replace(/&|<|>|"|'/g, function(match) {
+		
+		return replaceObj[match];
+		
+	});
+	
+	console.log(res);
+	
+}
+
+convertHTML("Dolce & Gabbana");
+
+convertHTML("Dolce & Gabbana are > than 'Versace' but < than Channel");
+
+convertHTML('This is "SPARTA", right!');
+
